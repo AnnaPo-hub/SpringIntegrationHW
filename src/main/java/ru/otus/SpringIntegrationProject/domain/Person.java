70cecb75ffb7b5b94df9446292ea82fe05b3eac1
Anna Popova <anna.popova201909@gmail.com>
2021-09-12 21:59:55 +0300

    убрала сеттер у персоны

--- src/main/java/ru/otus/SpringIntegrationProject/domain/Person.java
+++ src/main/java/ru/otus/SpringIntegrationProject/domain/Person.java
@@ -3,15 +3,17 @@ package ru.otus.SpringIntegrationProject.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
-import lombok.Setter;
 
 @Data
 @AllArgsConstructor
 @Getter
-@Setter
 public class Person {
     private final String name;
     private boolean notManicured;
     private boolean withoutMakeUp;
     private int clientDebt;
+
+//    public static Person of (String name, boolean notManicured, boolean withoutMakeUp, int value){
+////        return new Person (name, notManicured, withoutMakeUp, value);
+//    }
 }
